35e047e0adc25a3172437a72202575524f42e7d8
geraldprambudi <geraldprambudi94@gmail.com>
2019-10-01 08:54:10 +0700

    #fix login registration

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,14 +2,28 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
 
-	public function profil() {
-		echo "hello my name is bewoksatukosong";
-		// $this->load->view('profil');
+	public function hello() {
+		echo "hello";
 	}
-
 }
